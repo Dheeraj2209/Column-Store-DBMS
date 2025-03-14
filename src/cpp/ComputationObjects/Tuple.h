656c7ffd49fe1d44f1d048c7c352dfb744be8ee8
pradyunuydarp <pradyundevarakonda24@gmail.com>
2025-03-14 09:48:05 +0530

    misc

---
+++ src/cpp/ComputationObjects/Tuple.h
@@ -0,0 +1,16 @@
+//
+// Created by Pradyun Devarakonda on 12/03/25.
+//
+
+#ifndef TUPLE_H
+#define TUPLE_H
+
+
+
+class Tuple {
+
+};
+
+
+
+#endif //TUPLE_H
---
+++ src/cpp/include/stream_includes.h
@@ -0,0 +1,9 @@
+//
+// Created by Pradyun Devarakonda on 12/03/25.
+//
+
+#ifndef STREAM_INCLUDES_H
+#define STREAM_INCLUDES_H
+#include <fstream>
+#include <sstream>
+#endif //STREAM_INCLUDES_H
